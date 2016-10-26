19f827bb3f439408e6986ef737df30d630c48113
EC2 Default User <ec2-user@ip-172-31-30-61.ec2.internal>
2016-10-26 13:56:55 +0000

    added hash lookup for defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
 class xtables_yum_rebuild (
   $noop_value = lookup('xtables_yum_rebuild::noop_value'),
   $packages = lookup('xtables_yum_rebuild::packages'),
+  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
+  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
+  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
 ) {
   package { $packages:
     noop   => $noop_value,
