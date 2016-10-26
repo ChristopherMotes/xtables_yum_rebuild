ff5523129bb4f57946e9c2b1e7e91681443ba277
EC2 Default User <ec2-user@ip-172-31-30-61.ec2.internal>
2016-10-26 15:07:22 +0000

    appearently, I need a long class on the difference between arrays and hashes in hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,21 @@
 class xtables_yum_rebuild (
-  $noop_value = lookup('xtables_yum_rebuild::noop_value'),
-  $packages = lookup('xtables_yum_rebuild::packages'),
-  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
-  $action_file = lookup('xtables_yum_rebuild::action_file', 'Hash'),
-  $action_script = lookup('xtables_yum_rebuild::action_script', 'Hash'),
+  $noop_value 		= lookup('xtables_yum_rebuild::noop_value'),
+  $packages 		= lookup('xtables_yum_rebuild::packages'),
+  $file_defaults 	= lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
+  $action_file 		= lookup('xtables_yum_rebuild::action_file', 'Hash', {"strategy" => "deep", "merge_hash_arrays" => true} ),
+  $action_script 	= lookup('xtables_yum_rebuild::action_script', 'Hash[String]'),
 ) {
   package { $packages:
-    noop   => $noop_value,
-    ensure => 'present'
-  }
+    noop   	=> $noop_value,
+    ensure 	=> 'present'
+  } # end packages
+  file { 'xtables_action_file':
+    noop   	=> $noop_value,
+    path	=> $action_file['path'],
+    source	=> $action_file['source'],
+    ensure 	=> $file_defaults['ensure'],
+    owner  	=> $file_defaults['owner'],
+    group  	=> $file_defaults['group'],
+    mode   	=> $file_defaults['mode'],
+  } # end file xtables_action_file
 }
