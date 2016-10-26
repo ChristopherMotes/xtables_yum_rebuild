091630f97cec75b1932a06b4ccdc057384c19cbe
EC2 Default User <ec2-user@ip-172-31-30-61.ec2.internal>
2016-10-26 13:58:39 +0000

    added params for action file and script

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class xtables_yum_rebuild (
   $noop_value = lookup('xtables_yum_rebuild::noop_value'),
   $packages = lookup('xtables_yum_rebuild::packages'),
   $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
-  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
-  $file_defaults = lookup('xtables_yum_rebuild::file_defaults', 'Hash'),
+  $action_file = lookup('xtables_yum_rebuild::action_file', 'Hash'),
+  $action_script = lookup('xtables_yum_rebuild::action_script', 'Hash'),
 ) {
   package { $packages:
     noop   => $noop_value,
